5cc8068273263557008be6bdd0ae678f061d67d2
garethTurner <gareth.turner@ons.gov.uk>
2020-01-29 14:10:45 +0000

    initial setup

---
+++ src/main/java/uk/gov/ons/fsdr/report/FsdrReportWriterApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.ons.fsdr.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsdrReportWriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsdrReportWriterApplication.class, args);
+	}
+
+}
